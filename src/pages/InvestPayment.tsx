5d483462c85bf45ab74e8c50a204d0cca7fbfa36
Justfriends-hub <oladunnilawal1400@gmail.com>
2026-04-28 11:53:48 +0100

    Update account details

--- src/pages/InvestPayment.tsx
+++ src/pages/InvestPayment.tsx
@@ -118,8 +118,8 @@ const InvestPayment = () => {
               <p className="text-sm font-semibold">Bank Details</p>
               <div className="space-y-1 text-sm">
                 <div className="flex items-center justify-between">
-                  <p className="font-mono">Account:6448211839</p>
-                  <CopyButton text="6448211839" />
+                  <p className="font-mono">Account:6651047081</p>
+                  <CopyButton text="6651047081" />
                 </div>
                 <p>Name: Grace Onuoha</p>
                 <p>Bank: Nombank </p>
